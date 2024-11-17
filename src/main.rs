cb65de9d31321dfb1a32dfd0c78fb09359f1aa2b
rusty <veggie@tutamail.com>
2024-11-16 19:37:33 -0500

    change Command -> Instruction, enums out of main, async spawn stub

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,34 @@ use chrono::SubsecRound;
 use chrono::TimeDelta;
 use serde_json::Value;
 
+#[derive(Debug, Clone)]
+pub enum Instruction {
+    Lighting(Lights),
+    Pumping(Pump),
+}
+
+#[derive(Debug, Clone)]
+pub enum Lights {
+    LightsOn(DateTime<Local>),
+    LightsOff(DateTime<Local>),
+}
+
+#[derive(Debug, Clone)]
+pub enum Pump {
+    PumpOn(DateTime<Local>),
+    PumpOff(DateTime<Local>),
+}
+
+impl Instruction {
+    pub fn inspect(self) -> DateTime<Local> {
+        match self {
+            Instruction::Pumping(Pump::PumpOn(x)) => x,
+            Instruction::Pumping(Pump::PumpOff(x)) => x,
+            Instruction::Lighting(Lights::LightsOn(x)) => x,
+            Instruction::Lighting(Lights::LightsOff(x)) => x,
+        }
+    }
+}
 #[tokio::main]
 async fn main() -> () {
     // WARN while program is running, changing the clock, timezone, or daylight savings could cause problems
@@ -16,36 +44,8 @@ async fn main() -> () {
         current_datetime
     );
 
-    #[derive(Debug, Clone)]
-    pub enum Command {
-        Lighting(Lights),
-        Pumping(Pump),
-    }
+    let mut _command_schedule: Vec<Instruction> = Vec::new();
 
-    #[derive(Debug, Clone)]
-    pub enum Lights {
-        LightsOn(DateTime<Local>),
-        LightsOff(DateTime<Local>),
-    }
-
-    #[derive(Debug, Clone)]
-    pub enum Pump {
-        PumpOn(DateTime<Local>),
-        PumpOff(DateTime<Local>),
-    }
-
-    impl Command {
-        pub fn inspect(self) -> DateTime<Local> {
-            match self {
-                Command::Pumping(Pump::PumpOn(x)) => x,
-                Command::Pumping(Pump::PumpOff(x)) => x,
-                Command::Lighting(Lights::LightsOn(x)) => x,
-                Command::Lighting(Lights::LightsOff(x)) => x,
-            }
-        }
-    }
-
-    let mut _command_schedule: Vec<Command> = Vec::new();
     // TODO JSON file for schedule (MVP)
     let contents = tokio::fs::read_to_string("demo.json")
         .await
@@ -65,65 +65,83 @@ async fn main() -> () {
     // for now, we will hard code a single day demo schedule
     // multi day schedules could be generated algorithmically, ie, same for 12 days or, reduce light by 5/min a day for 40 days, etc
     // would this be good as a test?
-    let lights_off_time = Command::Lighting(Lights::LightsOff(
+    let lights_off_time = Instruction::Lighting(Lights::LightsOff(
+        current_datetime
+            .round_subsecs(0)
+            .checked_add_signed(TimeDelta::seconds(25))
+            .expect("pump off init to work"),
+    ));
+    let lights_on_time = Instruction::Lighting(Lights::LightsOn(
         current_datetime
             .round_subsecs(0)
             .checked_add_signed(TimeDelta::seconds(15))
             .expect("pump off init to work"),
     ));
-    let lights_on_time = Command::Lighting(Lights::LightsOn(
-        current_datetime
-            .round_subsecs(0)
-            .checked_add_signed(TimeDelta::seconds(5))
-            .expect("pump off init to work"),
-    ));
-    let pump_on_time = Command::Pumping(Pump::PumpOn(
-        current_datetime
-            .round_subsecs(0)
-            .checked_add_signed(TimeDelta::seconds(10))
-            .expect("pump off init to work"),
-    ));
-    let pump_off_time = Command::Pumping(Pump::PumpOff(
+    let pump_on_time = Instruction::Pumping(Pump::PumpOn(
         current_datetime
             .round_subsecs(0)
             .checked_add_signed(TimeDelta::seconds(20))
             .expect("pump off init to work"),
     ));
+    let pump_off_time = Instruction::Pumping(Pump::PumpOff(
+        current_datetime
+            .round_subsecs(0)
+            .checked_add_signed(TimeDelta::seconds(30))
+            .expect("pump off init to work"),
+    ));
 
     // manual display, but to show user would also require similar shinanigans
-    println!("pump off! @ {:#?}", Command::inspect(pump_off_time.clone()));
-    println!("pump on! @ {:#?}", Command::inspect(pump_on_time.clone()));
+    println!(
+        "pump off! @ {:#?}",
+        Instruction::inspect(pump_off_time.clone())
+    );
+    println!(
+        "pump on! @ {:#?}",
+        Instruction::inspect(pump_on_time.clone())
+    );
     println!(
         "lights off! @ {:#?}",
-        Command::inspect(lights_off_time.clone())
+        Instruction::inspect(lights_off_time.clone())
     );
     println!(
         "lights on! @ {:#?}",
-        Command::inspect(lights_on_time.clone())
+        Instruction::inspect(lights_on_time.clone())
     );
     //TODO pretty print upcoming schedule instead
 
-    let mut demo_schedule: Vec<Command> =
+    let mut demo_schedule: Vec<Instruction> =
         vec![lights_on_time, lights_off_time, pump_on_time, pump_off_time];
 
     println!("demo schedule init: {:?}", demo_schedule);
-    demo_schedule.sort_by(|a, b| Command::inspect(a.clone()).cmp(&Command::inspect(b.clone())));
+    demo_schedule
+        .sort_by(|a, b| Instruction::inspect(a.clone()).cmp(&Instruction::inspect(b.clone())));
     println!("demo schedule sorted: {:?}", demo_schedule);
 
-    // TODO now sorted into temporal order, parse into device cues
-    let mut light_schedule: Vec<Command> = Vec::new();
-    let mut pump_schedule: Vec<Command> = Vec::new();
+    // now sorted into temporal order, parse into device cues
+    let mut light_schedule: Vec<Instruction> = Vec::new();
+    let mut pump_schedule: Vec<Instruction> = Vec::new();
 
     for cmd in &demo_schedule {
         match cmd {
-            Command::Lighting(_) => light_schedule.push(cmd.clone()),
-            Command::Pumping(_) => pump_schedule.push(cmd.clone()),
+            Instruction::Lighting(_) => light_schedule.push(cmd.clone()),
+            Instruction::Pumping(_) => pump_schedule.push(cmd.clone()),
         }
     }
 
     println!("light schedule : {:?}", light_schedule);
     println!("pump schedule : {:?}", pump_schedule);
 
-    //TODO : actually send requests based on time
     //TODO : two async tasks: spawn, spawn + join
+    tokio::spawn(async move {
+        // Process each socket concurrently.
+        light_process(light_schedule).await
+    });
+
+    //calculate time within task?
+    //TODO : actually send requests based on time
+}
+
+async fn light_process(schedule: Vec<Instruction>) -> () {
+    // TODO add duration to systemttime
+    println!("inside light process")
 }
