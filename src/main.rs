69040a994e643abb8fce720d4a6d9230d33b4701
rusty <mineraa@tutamail.com>
2024-11-26 23:48:17 -0500

    tokio features, async main, get reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+extern crate tokio;
+
+#[tokio::main]
+async fn main() -> () {
+    let body = reqwest::get("https://docs.rs/").await.unwrap().text().await;
+    let resp = reqwest::get("http://127.0.0.1:9732").await;
+    println!("{body:#?}");
+    println!("{resp:#?}");
+    ()
 }
