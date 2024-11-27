16c34de32bbe443a308d53a73aa17d28d73d0878
rusty <mineraa@tutamail.com>
2024-11-26 23:54:32 -0500

    post reqwest

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,15 @@ extern crate tokio;
 
 #[tokio::main]
 async fn main() -> () {
-    let body = reqwest::get("https://docs.rs/").await.unwrap().text().await;
     let resp = reqwest::get("http://127.0.0.1:9732").await;
-    println!("{body:#?}");
     println!("{resp:#?}");
+
+    let post_client = reqwest::Client::new();
+    let res_one = post_client
+        .post("https://httpbin.org/post")
+        .body("this is the body one")
+        .send()
+        .await;
+    println!("res_one : {res_one:#?}");
     ()
 }
