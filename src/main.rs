5810ccbf9431bc6b6bdb975c9b5aed0c8e135a23
rusty <mineraa@tutamail.com>
2024-11-27 01:45:14 -0500

    async run_schedule demo with JoinSet

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use chrono::NaiveTime;
+use chrono::{NaiveTime, TimeDelta};
+
 use serde_json::Value;
+use tokio::{task::JoinSet, time::sleep, time::Duration};
 use url::Url;
 extern crate tokio;
 
@@ -25,6 +27,16 @@ async fn main() -> () {
     .expect("JSON Value works after read-in");
     println!("{:?}", json_contents);
 
+    // TODO ingest into structs
+
+    let mut set: JoinSet<()> = JoinSet::new();
+
+    // TODO just a demo
+    let devices = vec![1, 2];
+    for device in devices {
+        set.spawn(run_schedule(device));
+    }
+
     let resp = reqwest::get("http://127.0.0.1:9732").await;
     println!("{resp:#?}");
 
@@ -35,5 +47,15 @@ async fn main() -> () {
         .send()
         .await;
     println!("res_one : {res_one:#?}");
+    set.join_all().await;
     ()
+}
+
+// helper
+async fn run_schedule(n: usize) {
+    let demo_duration: Duration = Duration::from_secs(5);
+    loop {
+        println!("{:?}", n);
+        sleep(demo_duration).await;
+    }
 }
