2caf4f6a0cabf74f85bad8520cd93440c5a43568
rusty <mineraa@tutamail.com>
2024-11-27 03:03:51 -0500

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -30,24 +30,12 @@ async fn main() -> () {
     .expect("JSON Value works after read-in");
     println!("{:?}", devices);
 
-    // TODO ingest into structs
-
     let mut set: JoinSet<()> = JoinSet::new();
 
     for device in devices {
         set.spawn(run_schedule(device));
     }
 
-    let resp = reqwest::get("http://127.0.0.1:9732").await;
-    println!("{resp:#?}");
-
-    let post_client = reqwest::Client::new();
-    let res_one = post_client
-        .post("https://httpbin.org/post")
-        .body("this is the body one")
-        .send()
-        .await;
-    println!("res_one : {res_one:#?}");
     set.join_all().await;
     ()
 }
