d1b9e89171ba6bafbdf5f8c2f8fc2129e8d11fc8
rusty <mineraa@tutamail.com>
2024-11-30 16:24:06 -0500

    removed superfluous helper function, cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use chrono::{Local, NaiveTime, TimeDelta};
-
 use serde::Deserialize;
 use tokio::{task::JoinSet, time::sleep};
 use url::Url;
@@ -33,24 +32,17 @@ async fn main() {
     let mut set: JoinSet<()> = JoinSet::new();
 
     for device in devices {
-        set.spawn(launch_events_processes(device));
+        println!("working with device {:?}", device.name);
+        let evs = device.events.clone();
+        println!("events : {:?}", evs);
+
+        for e in evs {
+            println!("about to spawn 'e' on inner_set {:?}", e);
+            set.spawn(event_loop(device.clone(), e));
+        }
     }
 
     set.join_all().await;
-}
-
-async fn launch_events_processes(d: Device) {
-    let mut inner_set: JoinSet<()> = JoinSet::new();
-    // TODO  check for simultaneous events (prohibit or warn)
-    println!("working with device {:?}", d.name);
-    let evs = d.events.clone();
-    println!("events : {:?}", evs);
-
-    for e in evs {
-        println!("about to spawn 'e' on inner_set {:?}", e);
-        inner_set.spawn(event_loop(d.clone(), e));
-    }
-    inner_set.join_all().await;
 }
 
 async fn event_loop(d: Device, e: Event) {
@@ -59,13 +51,14 @@ async fn event_loop(d: Device, e: Event) {
         let mut diff: TimeDelta = e.timestamp - now;
         println!("diff : {:?}", diff);
 
-        // with naivetime for events, any given time will be before AND after now.
-        // (it will be each time the same event loops around, and in some initial conditions)
+        // with NaiveTime for events, any given time will be before AND after now.
+        // Negative time occurs each time an event fires and is asked to sleep
+        // until the same NaiveTime, and in some initial conditions.
         if diff.num_nanoseconds().expect(
             "num_nanoseconds to work [2^63 nano overflow.]",
-            // fun fact number of nanoseconds in a day is 8.64 x 10^13
+            // fun fact: the number of nanoseconds in a day is only 8.64 x 10^13
         ) < 0
-        // then we must add 24 hours to the wait time.
+        // then, we must add 24 hours to the wait time.
         {
             println!("negative time, about to add 24 hours. diff : {:?}", diff);
             diff += TimeDelta::days(1);
@@ -83,5 +76,6 @@ async fn event_loop(d: Device, e: Event) {
             let resp = reqwest::get(d.off.clone()).await;
             println!("Off ! {:#?}", resp);
         }
+        // and the loop will go on forever.
     }
 }
